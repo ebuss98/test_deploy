c5b508cf1ff6f9399df2c5f4a39961471130cd56
Евгений Бусс <evgenijbuss@MacBook-Pro-Evgenij-2.local>
2020-04-15 18:33:50 +0300

    make it better

---
+++ app.js
@@ -0,0 +1,9 @@
+const Telegraf = require('telegraf');
+
+const bot = new Telegraf('1202224321:AAETcN_RMPeY6mYXDQzFuTfzhfq6pRxKMu8');
+bot.start((ctx) => ctx.reply('Welcome!'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.launch();
